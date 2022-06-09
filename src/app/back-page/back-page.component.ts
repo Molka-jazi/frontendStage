c089fd74d0ddaefd9d8a0941587527264951d159
Molka-jazi <molka.eljazi@esprit.tn>
2022-06-09 16:09:14 +0100

    interface login

---
+++ src/app/back-page/back-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-back-page',
+  templateUrl: './back-page.component.html',
+  styleUrls: ['./back-page.component.css']
+})
+export class BackPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
