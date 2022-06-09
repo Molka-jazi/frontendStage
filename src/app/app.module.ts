92cc40914f9d1e57a01539e8167c884a59d28608
Molka-jazi <molka.eljazi@esprit.tn>
2022-06-09 16:12:43 +0100

    interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BackPageComponent } from './back-page/back-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackPageComponent
   ],
   imports: [
     BrowserModule
